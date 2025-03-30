df3d7ce378a49161739ea32300d9fabc3f8dc3be
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-03-30 19:31:48 -0300

    Ícone TechStore

--- src/app/products/_components/navbar.tsx
+++ src/app/products/_components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FaStore } from "react-icons/fa";
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -26,7 +27,11 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="flex justify-end items-center p-4">
+    <nav className="flex justify-between items-center p-4">
+      <div className="flex items-center">
+        <FaStore className="text-4xl text-white" />
+        <span className="ml-2 text-2xl font-bold text-white">TechStore</span>
+      </div>
       <div className="flex items-center gap-4 font-bold">
         <button
           className="bg-red-500/40 text-white px-4 py-2 rounded-md hover:bg-red-500/60 transition-all duration-300"
